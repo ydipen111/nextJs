44c8dc1e1a91dc7ed463092e74206ecc35167160
Your Name <aggressivebanda74@gmail.com>
2025-01-08 23:25:49 +0545

    fix(@revenue): adding parralel rougin in c_dashboard

--- app/c_dashboard/layout.tsx
+++ app/c_dashboard/layout.tsx
@@ -1,12 +1,25 @@
+"use client"
 import React, { ReactNode } from 'react'
 
-export default function layout({ children, }: {
-  children: ReactNode
+export default function layout({
+  children,
+  notifications,
+  user,
+  revenue
+}: {
+  children: ReactNode,
+  notifications: ReactNode,
+  user: ReactNode,
+  revenue: ReactNode
 }) {
   return (
     <div>
       Complex dashboard
-      {children}
+      <h1>{children}</h1>
+      <h3>{notifications}</h3>
+      <h3>{user}</h3>
+      <h3>{revenue}</h3>
+      <h3></h3>
 
     </div>
   )
